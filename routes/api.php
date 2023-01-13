d8e59ec82df818d7ed021cc940a2cbaa1ba8085d
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-01-13 18:44:44 +0100

    Posibilitando el resto de request sobre migrations

--- routes/api.php
+++ routes/api.php
@@ -32,14 +32,13 @@ Route::any('/{any}', function (ServerRequestInterface $request) {
     ]);
     $api = new Api($config);
     $response = $api->handle($request);
-/* Para RESTED
-    return $response;
- */
 
-/* Para REACT-ADMIN */
-    $records = json_decode($response->getBody()->getContents())->records;
-    $response = response()->json($records, 200, $headers = ['X-Total-Count' => count($records)]);
+    try {
+        $records = json_decode($response->getBody()->getContents())->records;
+        $response = response()->json($records, 200, $headers = ['X-Total-Count' => count($records)]);
+    } catch (\Throwable $th) {
+
+    }
     return $response;
-/* Fin REACT-ADMIN */
 
 })->where('any', '.*');
