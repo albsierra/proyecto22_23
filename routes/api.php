f14547a2060e0d48fdf5867d80988960e3fb8422
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-01-22 10:11:02 +0100

    Generando tokens para el acceso REST

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,10 @@ use Psr\Http\Message\ServerRequestInterface;
 use App\Http\Controllers\API\CustomerController;
 use App\Http\Controllers\API\UserController;
 
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 use Tqdev\PhpCrudApi\Api;
 use Tqdev\PhpCrudApi\Config\Config;
 
@@ -22,12 +26,34 @@ use Tqdev\PhpCrudApi\Config\Config;
 */
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    $user = $request->user();
+    $user->fullName = $user->name;
+    return $user;
 });
 
 Route::apiResource('customers', CustomerController::class);
 
 Route::apiResource('users', UserController::class);
+
+Route::post('/tokens/create', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json([
+        'token_type' => 'Bearer',
+        'access_token' => $user->createToken('token_name')->plainTextToken // token name you can choose for your self or leave blank if you like to
+    ]);
+});
 
 Route::any('/{any}', function (ServerRequestInterface $request) {
     $config = new Config([
