f3f3e4c0f7ae4fbed1c5446d52c353e3cd26c9f9
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-01-31 10:06:17 +0100

    Haciendo que se busque en varias propiedades del recurso

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -18,8 +18,20 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $numElementos = $request->input('numElements');
+        $busquedaArray = [
+            'name',
+            'email',
+        ];
+        $busquedaFiltroQ = $request->input('filter');
+        $registrosUsuario = User::query();
+        if($busquedaFiltroQ && array_key_exists('q', $busquedaFiltroQ)) {
+            foreach ($busquedaArray as $fieldName) {
+                $registrosUsuario = $registrosUsuario
+                    ->orWhere($fieldName, 'like', '%' .$busquedaFiltroQ['q'] . '%');
+            }
+        }
 
-        return UserResource::collection(User::paginate($numElementos));
+        return UserResource::collection($registrosUsuario->paginate($numElementos));
     }
 
     /**
