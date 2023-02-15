55cee9863a04f4f9f13b76c15fd0621079d238e7
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-02-15 17:25:44 +0100

    Desplegando React en Laravel

---
+++ public/js/main.js
@@ -0,0 +1,96 @@
+/*
+	Directive by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$menu = $('#menu'),
+		$sidebar = $('#sidebar'),
+		$main = $('#main');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Menu.
+		$menu
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right',
+				target: $body,
+				visibleClass: 'is-menu-visible'
+			});
+
+	// Search (header).
+		var $search = $('#search'),
+			$search_input = $search.find('input');
+
+		$body
+			.on('click', '[href="#search"]', function(event) {
+
+				event.preventDefault();
+
+				// Not visible?
+					if (!$search.hasClass('visible')) {
+
+						// Reset form.
+							$search[0].reset();
+
+						// Show.
+							$search.addClass('visible');
+
+						// Focus input.
+							$search_input.focus();
+
+					}
+
+			});
+
+		$search_input
+			.on('keydown', function(event) {
+
+				if (event.keyCode == 27)
+					$search_input.blur();
+
+			})
+			.on('blur', function() {
+				window.setTimeout(function() {
+					$search.removeClass('visible');
+				}, 100);
+			});
+
+	// Intro.
+		var $intro = $('#intro');
+
+		// Move to main on <=large, back to sidebar on >large.
+			breakpoints.on('<=large', function() {
+				$intro.prependTo($main);
+			});
+
+			breakpoints.on('>large', function() {
+				$intro.prependTo($sidebar);
+			});
+
+})(jQuery);
